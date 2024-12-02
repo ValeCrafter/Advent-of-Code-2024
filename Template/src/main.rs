00f30857664836b181a934079ac234765395fe24
¨ValeCrafter¨ <valerio04@gmx.ch>
2024-12-02 22:49:21 +0100

    template unfucked again

--- Template/src/main.rs
+++ Template/src/main.rs
@@ -5,8 +5,5 @@ fn main() {
     let input_file = fs::read_to_string("input.txt").unwrap();
     let mut output: String = "empty".to_string();
 
-    // spallte splitte
-
-
     print!("{}", output);
 }
