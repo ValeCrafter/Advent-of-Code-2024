de34923976dd2b440ba750a6036e5281fa08ea18
¨ValeCrafter¨ <valerio04@gmx.ch>
2024-12-02 23:11:38 +0100

    and again

--- Template/src/main.rs
+++ Template/src/main.rs
@@ -5,5 +5,7 @@ fn main() {
     let input_file = fs::read_to_string("input.txt").unwrap();
     let mut output: String = "empty".to_string();
 
-    print!("{}", output);
+
+    
+    println!("{}", output);
 }
