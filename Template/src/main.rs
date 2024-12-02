57d07593c7dbd96da5a4cc6a90f30926bad6a987
¨ValeCrafter¨ <valerio04@gmx.ch>
2024-12-02 22:48:39 +0100

    template unfucked

--- Template/src/main.rs
+++ Template/src/main.rs
@@ -1,20 +1,12 @@
 
 use std::fs;
-use std::error::Error;
 
 fn main() {
-    let mut input_file = &read_input_file();
+    let input_file = fs::read_to_string("input.txt").unwrap();
     let mut output: String = "empty".to_string();
 
-
-
-    output = input_file.to_string();
-
+    // spallte splitte
 
 
     print!("{}", output);
-}
-
-fn read_input_file() -> String{
-    return fs::read_to_string("input.txt").unwrap();
 }
