89a756874e075bc06ceb4b1ac298d130de2edbc9
ValeCrafter <valerio04@gmx.ch>
2024-12-03 08:24:31 +0100

    Updated Template

--- Template/src/main.rs
+++ Template/src/main.rs
@@ -1,11 +1,19 @@
-
 use std::fs;
 
 fn main() {
-    let input_file = fs::read_to_string("input.txt").unwrap();
-    let mut result: i32 = 0;
-    
+    let input = fs::read_to_string("input.txt").unwrap();
+    part1(&input);
+    part2(&input);
+}
 
+fn part1(input: &str){
+    let mut sum = 0;
     
-    println!("{}", result);
+    println!("Part1: {}", sum);
+}
+
+fn part2(input: &str){
+    let mut sum = 0;
+
+    println!("Part2: {}", sum);
 }
