657dd341402ee06f9d157975c07deaa527810214
¨ValeCrafter¨ <valerio04@gmx.ch>
2024-12-05 21:18:42 +0100

    Day4 Part 2

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -47,7 +47,7 @@ fn check_around(lines: &Vec<Vec<char>>, seach_char: char,y: i32, x: i32) -> Vec<
     let offsets  = vec![(-1,-1), (-1,0), (-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)];
     let mut result: Vec<(i32, i32)> = Vec::new(); 
     for offset in offsets{
-        if check_direction(&lines, 'M', y,x,offset.0, offset.1){
+        if check_direction(&lines, seach_char, y,x,offset.0, offset.1){
            result.push((offset.0, offset.1));
         }
     }
@@ -66,6 +66,43 @@ fn check_direction(lines: &Vec<Vec<char>>,seach_char: char,y: i32, x: i32,y_offs
 
 fn part2(input: &str){
     let mut sum = 0;
+
+    let mut lines: Vec<Vec<char>> = input.lines()
+                                .map(|s|   
+                                    s.chars()
+                                    .collect()
+                                            )
+                                .collect();
+    for y in 0..lines.len(){
+        //println!("{:?}", lines[y]);
+        for x in 0..lines[0].len(){
+            let x_char = lines[y][x];
+            if x_char == 'A'{
+                let m_positions: Vec<(i32, i32)> = check_around(&lines, 'M', y as i32, x as i32)
+                    .iter()
+                    .filter(|p| p.0 != 0 && p.1 != 0)
+                    .copied()
+                    .collect();
+                let s_positions: Vec<(i32, i32)>  = check_around(&lines, 'S', y as i32, x as i32)
+                    .iter()
+                    .filter(|p| p.0 != 0 && p.1 != 0)
+                    .copied()
+                    .collect();
+
+                if m_positions.len() == 2 && s_positions.len() == 2 {
+
+                    let expectec_s_positions: Vec<(i32, i32)> = m_positions.iter().map(|m| (m.0*-1,m.1*-1)).collect();
+
+                    if expectec_s_positions[0].0 == s_positions[1].0 
+                        && expectec_s_positions[0].1 == s_positions[1].1
+                        && expectec_s_positions[1].0 == s_positions[0].0
+                        && expectec_s_positions[1].1 == s_positions[0].1{
+                        sum += 1;
+                    }
+                }
+            }
+        }
+    }
 
     println!("Part2: {}", sum);
 }
