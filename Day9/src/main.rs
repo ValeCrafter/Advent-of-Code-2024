14f033614eb2a9ec969b31ba1c440584a382db90
ValeCrafter <valerio04@gmx.ch>
2024-12-12 09:18:35 +0100

    Day9 Part2

--- Day9/src/main.rs
+++ Day9/src/main.rs
@@ -48,5 +48,57 @@ fn part1(input: &str){
 fn part2(input: &str){
     let mut sum = 0;
 
+    
+    //fill vec
+    let mut drive= Vec::new();
+    for c_index in 0..input.len(){
+        let c: char = input.chars().collect::<Vec<char>>()[c_index];
+        if c_index % 2 == 0{
+            drive.push((Some(c_index as u64 / 2), c.to_digit(10).unwrap()));
+        } 
+        else{
+            drive.push((None, c.to_digit(10).unwrap()));
+        }
+    }
+
+    //sort
+    for i in 0..drive.len(){
+        let pos = drive.len() - 1usize - i;
+        let pos_item = drive[pos];
+
+        if pos_item.0.is_some(){
+            let empty_pos = drive.iter().position(|p| p.1 >= pos_item.1 && p.0.is_none());
+            match empty_pos{
+                Some(v) => {
+                    if v < pos{
+                        drive[pos].0 = None;
+                        if drive[v].1 == pos_item.1{
+                            drive.remove(v);
+                        }
+                        else{
+                            drive[v].1 -= pos_item.1;
+                            if drive[v].1 == 0{
+                                drive.remove(v);
+                            }
+                        }
+                        drive.insert(v,pos_item);
+                    }
+                }
+                None => ()
+            }
+        }
+    }
+
+    //calc
+    let mut index = 0;
+    for item in drive{
+        if item.0.is_some(){
+            for i in 0..item.1{
+                sum += item.0.unwrap() * (index + i) as u64;
+            }
+        }
+        index += item.1;
+    }
+
     println!("Part2: {}", sum);
 }
